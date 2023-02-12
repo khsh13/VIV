c452e488d65ef19a4cda2c6975a60b189d76367b
Khushi Sharma <96431469+khsh13@users.noreply.github.com>
2023-02-12 10:13:06 +0530

    Add files via upload

---
+++ solana.js
@@ -0,0 +1,17 @@
+function getAccount()
+{
+window.solana.connect();
+window.solana.request({ method: "connect" })
+  
+window.solana.isConnected
+window.solana.autoApproved
+
+  document.getElementById("user-account").innerHTML = window.solana.publicKey;
+}
+
+function disconnectAccount()
+{
+  window.solana.disconnect();
+  window.solana.on('disconnect', () => console.log("disconnected!"))
+}
+
